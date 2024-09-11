f0395bd572b45ff4dfb89769e7ba5a0c820fbae2
Shivam Taneja <shiivamtaneja@gmail.com>
2024-09-11 10:24:08 +0530

    add notification, ipinfo module to app.module
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import clerkConfig from './config/clerk.config';
-import { ResumeModule } from './resume/resume.module';
-import { CloudModule } from './cloud/cloud.module';
-import { OpenAIModule } from './openai/openai.module';
-import { PaymentsModule } from './payments/payments.module';
 import { AppController } from './app.controller';
+import { CloudModule } from './cloud/cloud.module';
+import clerkConfig from './config/clerk.config';
+import { IpInfoModule } from './ip-info/ip-info.module';
 import { JobsModule } from './jobs/jobs.module';
 import { LinkedinOptimizerModule } from './linkedin-optimizer/linkedin-optimizer.module';
-import { NotificationService } from './notification/notification.service';
 import { NotificationModule } from './notification/notification.module';
-import { IpInfoModule } from './ip-info/ip-info.module';
+import { NotificationService } from './notification/notification.service';
+import { OpenAIModule } from './openai/openai.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ResumeModule } from './resume/resume.module';
 
 @Module({
   imports: [
@@ -28,6 +28,8 @@ import { IpInfoModule } from './ip-info/ip-info.module';
     PaymentsModule,
     JobsModule,
     LinkedinOptimizerModule,
+    NotificationModule,
+    IpInfoModule,
   ],
   controllers: [AppController],
   providers: [NotificationService],
