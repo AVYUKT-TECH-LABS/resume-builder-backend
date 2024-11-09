9a650a70e9ef941313a54825394ce20c2eb0a1a5
Divyansh Gupta <divyansh@avyuktlabs.in>
2024-11-09 21:42:21 +0530

    linkedin login fix

--- src/auth/strategies/linkedin.strategy.ts
+++ src/auth/strategies/linkedin.strategy.ts
@@ -4,34 +4,31 @@ import { Strategy } from 'passport-linkedin-oauth2';
 
 @Injectable()
 export class LinkedinStrategy extends PassportStrategy(Strategy, 'linkedin') {
-  constructor() {
-    super({
-      clientID: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/v1/auth/linkedin/redirect`,
-      scope: ['openid', 'profile', 'email'],
-      state: true,
-    });
-  }
+    constructor() {
+        super({
+            clientID: process.env.LINKEDIN_CLIENT_ID,
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+            callbackURL: `${process.env.BACKEND_URL}/v1/auth/linkedin/redirect`,
+            scope: ['openid', 'profile', 'email'],
+            state: true,
+        });
+    }
 
-  async validate(
-    req: any,
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: (err, user) => void,
-  ): Promise<any> {
-    console.log(profile);
-    const { name, emails, photos } = profile;
-    console.log(profile);
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
-      accessToken,
-      refreshToken,
-    };
-    done(null, user);
-  }
+    async validate(
+        accessToken: string,
+        refreshToken: string,
+        profile: any,
+        done: (err, user) => void,
+    ): Promise<any> {
+        const { given_name, family_name, email, picture } = profile;
+        const user = {
+            email: email,
+            firstName: given_name,
+            lastName: family_name,
+            picture: picture,
+            accessToken,
+            refreshToken,
+        };
+        done(null, user);
+    }
 }
