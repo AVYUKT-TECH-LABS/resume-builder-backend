a450c7a7340fbdc88e18f76c12c34ec58ade8a8c
Divyansh Gupta <divyansh@avyuktlabs.in>
2024-11-19 17:33:12 +0530

    fix: memory leak

--- src/main.ts
+++ src/main.ts
@@ -6,74 +6,78 @@ import { AppModule } from './app.module';
 import _puppeteer from './puppeteer';
 import session from 'express-session';
 import * as bodyParser from 'body-parser';
+import memoryStore from 'memorystore';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    const app = await NestFactory.create(AppModule);
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  // Define the regex patterns based on the environment
-  const allowedOriginRegexDev = /^https:\/\/(?:[\w-]+\.)*talentxcel\.net$/;
-  const allowedOriginRegexProd = /^https:\/\/talentxcel\.net$/;
+    // Define the regex patterns based on the environment
+    const allowedOriginRegexDev = /^https:\/\/(?:[\w-]+\.)*talentxcel\.net$/;
+    const allowedOriginRegexProd = /^https:\/\/talentxcel\.net$/;
 
-  // Check the environment
-  const isDevelopment = process.env.NODE_ENV === 'development';
+    // Check the environment
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    const MemoryStore = memoryStore(session);
+    app.use(
+        session({
+            secret: 'session-secret',
+            resave: false,
+            saveUninitialized: false,
+            store: new MemoryStore({
+                checkPeriod: 86400000 // prune expired entries every 24h
+            }),
+        }),
+    );
 
-  app.use(
-    session({
-      secret: 'session-secret',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
+    app.enableCors({
+        origin: (origin, callback) => {
+            const allowedOrigins = [
+                'http://localhost:3001',
+                ...(isDevelopment ? [] : ['https://talentxcel.net']),
+            ];
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:3001',
-        ...(isDevelopment ? [] : ['https://talentxcel.net']),
-      ];
+            const allowedOriginRegex = isDevelopment
+                ? allowedOriginRegexDev
+                : allowedOriginRegexProd;
 
-      const allowedOriginRegex = isDevelopment
-        ? allowedOriginRegexDev
-        : allowedOriginRegexProd;
+            // Allow requests with no origin (e.g., mobile apps, curl)
+            if (
+                !origin ||
+                allowedOrigins.includes(origin) ||
+                allowedOriginRegex.test(origin)
+            ) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    });
 
-      // Allow requests with no origin (e.g., mobile apps, curl)
-      if (
-        !origin ||
-        allowedOrigins.includes(origin) ||
-        allowedOriginRegex.test(origin)
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    });
 
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
+    app.use(cookieParser());
 
-  app.use(cookieParser());
+    const config = new DocumentBuilder()
+        .setTitle('Resume Builder')
+        .setDescription('The resume builder API description')
+        .setVersion('1.0')
+        .addCookieAuth()
+        .build();
 
-  const config = new DocumentBuilder()
-    .setTitle('Resume Builder')
-    .setDescription('The resume builder API description')
-    .setVersion('1.0')
-    .addCookieAuth()
-    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  const port = process.env.PORT || 3000;
-  await _puppeteer();
-  await app.listen(port);
+    const port = process.env.PORT || 3000;
+    await _puppeteer();
+    await app.listen(port);
 }
 
 bootstrap();
